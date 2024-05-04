f8f052d820ccc578464406b5e992d299284cadcb
Obinna Odirionye <odirionye@gmail.com>
2024-05-04 22:47:13 +0400

    chore(cmd): Update triggr CLI command descriptions and examples

--- cmd/triggr.go
+++ cmd/triggr.go
@@ -22,7 +22,7 @@ var installCmd = &cobra.Command{
 	Use:     "install",
 	Aliases: []string{"i"},
 	Short:   "Install SSH Public Key",
-	Long:    `Install triggr's SSH public key on your server to enable secure communication between your server and the Clouddley Platform. For example:`,
+	Long:    `Install triggr's SSH public key on your server to enable secure communication between your server and the Clouddley Platform.`,
 	Example: "clouddley triggr install",
 	Run: func(cmd *cobra.Command, args []string) {
 		sshPublicKey := `ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCYDppnNM+F+GtFWaVJXsvobX/i/2uZuLch9386ZEyVtGE1QmRjRkvwEHwFM23STuzbtmqTrYjEnmv3Xkywk7wE0r+OoJxTBIwJP+scg9rAu//3N6CoAKH0Ra1XgdRj8QzqF/1mm4T/Pxtzz3JSpSKwpzW3GtU4NcHuaPAAHavCpahCnZqpPMU90FgRCS9lSmw0EPQcU8kxxeEpFjifip4JBBx/WQuh/8KkBAX/DnWSAO9ynGzPMvOvWPTtQMi7IA7Y8vRWeThfpC/fnU8Tub+99w5h2Y1TnWtUrM49ZMa9WSLtP/+4xKieQPObq0JuX6itNFuuwbb/WHLgOYeZqQTdSeMc6GlSkqniYiAUAv7olBUERHf7QkD7hPOlaw9S/0MCU8DcuujZG2i6UvIkQ60dikvsX8rCiPvfN4Nw1mWh0a1rf9vUxTyCCb+7hh1iPV6RwMx6T4nBjFNjBglHFkYIE5kevLyX2vREJJen+GfZO2GVcnHaNRHBvXZVVEbwt1xRWhAOS+FFtcKUNV+54JsKTaZUEYvfwe/KNjEeOxucljkiK9IYw0IGXB9dtueOTKcirLhpGE9t6LqDhWE05kr0fl/hmnT/g9fHeZDm4jOF71iHogsrZtU5pH8QtTNhaffMkW4EJc+4W0a+boE+/S5Xracbr7D1WBhGC2epXkUWHw== “clouddley-triggr-public-key”`
